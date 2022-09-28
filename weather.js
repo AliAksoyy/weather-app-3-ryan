1fd383043523f478e39bbbf3bb7cdf11d46fcb00
Ali <alifeyza4017@gmail.com>
2022-09-28 20:47:33 +0300

    added

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-const form = document.querySelector(".section.top-banner form")
+const form = document.querySelector("section.top-banner form")
 const input = document.querySelector(".container input")
 const msg = document.querySelector(".span.msg")
 const list = document.querySelector(".ajax-section .cities")
@@ -11,3 +11,17 @@ localStorage.setItem(
 //   "tokenKeyEncrypted",
 //   EncryptStringAES("5bb1e360c88ace5e3a7f0c8cfe9a34d7")
 // );
+
+form.addEventListener("submit", (e)=> {
+        e.preventDefault();
+    getWeatherDataFromApi()
+})
+
+//* get api func. (http method == verbs)
+
+const getWeatherDataFromApi = ()=> {
+        // alert("http request is gone")
+      const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"))
+      console.log(tokenKey)
+}
+
