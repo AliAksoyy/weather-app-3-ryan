72ace02fa4e9860b804cb7b0799026610e84dc19
Ali <alifeyza4017@gmail.com>
2022-09-28 21:29:57 +0300

    added

--- weather.js
+++ weather.js
@@ -26,15 +26,33 @@ const getWeatherDataFromApi = async ()=> {
 
       const inputValue = input.ariaValueMax;
       const units = "metric"
-      const lang = tr
+      const lang = "tr"
 
       const url = `https://api.openweathermap.org/data/2.5/weather?=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
 
-        const responsive = await fetch(url).then((res)=> res.json())
-        console.log(responsive)
+       const response = await fetch(url).then(responsive=>responsive.json())
+       console.log(response)
+
+    const {main,sys,weather,name} = response
+
+       const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
         
       
+       const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
 
+       const createdLi =document.createElement("li")
+       createdLi.className = "city"
+    createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
+      sys.country
+    }">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+            <figure>
+                <img class="city-icon" src="${iconUrl}">
+                <figcaption>${weather[0].description}</figcaption>
+            </figure>`;
 
 }
 
