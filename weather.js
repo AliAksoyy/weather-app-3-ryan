8eb314dd83884901c243afa891dc7e7fc6449abf
Ali <alifeyza4017@gmail.com>
2022-09-28 20:28:28 +0300

    added

--- weather.js
+++ weather.js
@@ -3,4 +3,11 @@ const input = document.querySelector(".container input")
 const msg = document.querySelector(".span.msg")
 const list = document.querySelector(".ajax-section .cities")
 
-localStorage.setItem("tokenKey", "5bb1e360c88ace5e3a7f0c8cfe9a34d7");
+localStorage.setItem(
+  "tokenKey",
+  "pc8YHOgPN1XTHnmt2lLm/vIGUAaGJFJ1vEABsIprjGvkdyLesISAotmAipoCW2bg"
+);
+// localStorage.setItem(
+//   "tokenKeyEncrypted",
+//   EncryptStringAES("5bb1e360c88ace5e3a7f0c8cfe9a34d7")
+// );
