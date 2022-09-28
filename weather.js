d31c621c54f273b512b730f6ee0f99e240e80e96
Ali <alifeyza4017@gmail.com>
2022-09-28 20:20:34 +0300

    added

---
+++ weather.js
@@ -0,0 +1,6 @@
+const form = document.querySelector(".section.top-banner form")
+const input = document.querySelector(".container input")
+const msg = document.querySelector(".span.msg")
+const list = document.querySelector(".ajax-section .cities")
+
+localStorage.setItem("tokenKey", "5bb1e360c88ace5e3a7f0c8cfe9a34d7");
