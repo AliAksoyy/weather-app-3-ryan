8fbaf904649142dba0efe85adcaf5ce8a1f36e44
Ali <alifeyza4017@gmail.com>
2022-09-29 08:13:27 +0300

    added

--- weather.js
+++ weather.js
@@ -1,2 +1,12 @@
 
 
+document.querySelector("#sports").addEventListener("click", function(e) {
+  console.log(e)
+  console.log(e.target.getAttribute("id") + ' is clicked')
+
+  const target = e.target
+
+  if(target.matches("li")) {
+    target.style.backgroundColor = "lightgrey"
+  }
+})
